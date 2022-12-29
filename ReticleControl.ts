8159b54175a28e14586a40e05e90516807652965
Alexey Belkin <alexey.belkin@mail.ru>
2022-12-30 02:15:25 +0300

    cleanup code, improve typings

--- ReticleControl.ts
+++ ReticleControl.ts
@@ -1,4 +1,7 @@
 ﻿namespace L {
+    export interface Map extends Evented {
+        reticleControl: L.ReticleControl
+    }
     export class RecticleControlOptions implements ControlOptions {
         position: L.ControlPosition = 'absolute'
     }
@@ -8,11 +11,11 @@
         _container: HTMLDivElement
         _previousWidthDist?: number
         _previousHeightDist?: number
-        constructor(options: L.RecticleControlOptions) {
+        constructor(options?: L.RecticleControlOptions) {
             options = { ...new L.RecticleControlOptions(), ...options}
             super(options);
         }
-        onAdd (map) {
+        override onAdd (map: L.Map): HTMLElement {
             this._map = map;
             map.reticleControl = this;
             map.on('themechanged', this._updateReticle, this);
@@ -31,17 +34,14 @@
             return this._container;
         }
 
-        onRemove(map) {
-        }
-
-        show (e) {
+        show(e: L.LayersControlEvent & { layer: L.Layer & { options: any } }) {
             if (e.layer && e.layer.options && e.layer.options.id === 'SC') {
                 this.isVisible = true;
                 this._updateReticle();
                 this._container.style.visibility = 'visible';
             };
         }
-        hide (e) {
+        hide(e: L.LayersControlEvent & { layer: L.Layer & { options: any } }) {
             if (e.layer && e.layer.options && e.layer.options.id === 'SC') {
                 this.isVisible = false;
                 this._container.innerHTML = '';
@@ -52,7 +52,7 @@
             const self = this;
             setTimeout(() => L.ReticleControl.prototype._updateReticle.call(self), 50);
         }
-        _updateReticle(context?: L.ReticleControl) {
+        _updateReticle(context?: L.ReticleControl | {new:string, old: string}) {
             const self: L.ReticleControl = this || context;
             if (!self._map.hasLayer(suncalc)) return;
 
@@ -61,7 +61,7 @@
             const _HALF = 1 / 2;
             const _QUARTER = 1 / 4;
             const _THREE_QUARTERS = 3 / 4;
-            function _getScaleRatioLabel(maxDist): [number, string] {
+            function _getScaleRatioLabel(maxDist: number): [number, string] {
                 let roundDist, unitStr;
                 if (maxDist > 1000) {
                     maxDist = maxDist / 1000;
@@ -73,26 +73,26 @@
                 }
                 return [roundDist / maxDist, `${roundDist} ${unitStr}`];
             }
-            function getRect(xStart, yStart, width, height) {
+            function getRect(xStart: number, yStart: number, width: number, height: number) {
                 return `<rect class="gis-themeaware reticle ${theme}" x=${xStart} y="${yStart}" width="${width}" height="${height}"></rect>`;
             }
-            function _calculateMaxDistance(xS, yS, xE, yE) {
+            function _calculateMaxDistance(xS: number, yS: number, xE: number, yE: number) {
                 return self._map.distance(
                     self._map.containerPointToLatLng([xS, yS]),
                     self._map.containerPointToLatLng([xE, yE])
                 );
             }
 
-            function _getRoundNum(num) {
+            function _getRoundNum(num: number) {
                 let pow10 = Math.pow(10, `${Math.floor(num)}`.length - 1);
                 let d = num / pow10;
                 d = d >= 10 ? 10 : d >= 5 ? 5 : d >= 3 ? 3 : d >= 2 ? 2 : 1;
                 return pow10 * d;
             }
 
-            let mapSize = self._map.getSize();
-            let mapWidthFromCenter = mapSize.x / 2;
-            let mapHeightFromCenter = mapSize.y / 2;
+            const mapSize: Point = self._map.getSize();
+            const mapWidthFromCenter: number = mapSize.x / 2;
+            const mapHeightFromCenter: number = mapSize.y / 2;
 
             const maxReticleWidth = Math.min(220, halfSize - 20);
             const maxReticleHeight = Math.min(200, halfSize - 30);
@@ -115,16 +115,16 @@
                 self._previousWidthDist = maxWidthDist;
             self._previousHeightDist = maxHeightDist;
 
-            let [widthRatio, widthText] = _getScaleRatioLabel(maxWidthDist);
-            let [heightRatio, heightText] = _getScaleRatioLabel(maxHeightDist);
+            const [widthRatio, widthText] = _getScaleRatioLabel(maxWidthDist);
+            const [heightRatio, heightText] = _getScaleRatioLabel(maxHeightDist);
             const widthLabel = `<text class="gis-themeaware reticle-text ${theme}" x="${halfSize + widthRatio * maxReticleWidth - 16}" y="${halfSize - 4}">${widthText}</text>`;
             const heightLabel = `<text class="gis-themeaware reticle-text ${theme}" x="${halfSize - 8}" y="${halfSize + heightRatio * maxReticleHeight + 12}">${heightText}</text>`;
 
-            function _widthTicks(ratio, tickHeightRatio?) {
+            function _widthTicks(ratio: number, tickHeightRatio?: number) {
                 tickHeightRatio = tickHeightRatio ? tickHeightRatio : 1;
                 return `${getRect(halfSize - 2 + widthRatio * maxReticleWidth * ratio, halfSize, 2, 8 * tickHeightRatio)}`
             }
-            function _heightTicks(ratio, tickWidthRatio?) {
+            function _heightTicks(ratio: number, tickWidthRatio?: number) {
                 tickWidthRatio = tickWidthRatio ? tickWidthRatio : 1;
                 return `${getRect(halfSize, halfSize - 2 + heightRatio * maxReticleHeight * ratio, 8 * tickWidthRatio, 2)}`
             }
@@ -135,7 +135,7 @@
         }
     };
     export namespace control {
-        export function reticle (opts) {
+        export function reticle (opts?: L.RecticleControlOptions) {
             return new L.ReticleControl(opts);
         }
     }
